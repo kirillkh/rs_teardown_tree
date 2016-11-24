dea7b83e950c71b39e26c6d49c58e073cf0c0d1e
Kirill Khazan <kirillkh@gmail.com>
2016-11-24 16:00:12 +0200

    imptree teardown

--- src/bin/benchmarks.rs
+++ src/bin/benchmarks.rs
@@ -141,6 +141,53 @@ fn imptree_delete_range_n(n: usize, rm_items: usize, iters: u64) {
     println!("average time to delete range of {} elements from implicit_tree of {} elements: {}ns", rm_items, n, elapsed_nanos/iters)
 }
 
+#[inline(never)]
+fn imptree_teardown_cycle(n: usize, rm_items: usize, iters: u64) {
+    let mut rng = XorShiftRng::from_seed([1,2,3,4]);
+    let elems: Vec<_> = (0..n).collect();
+
+    let nranges = n / rm_items +
+        if n % rm_items != 0 { 1 } else { 0 };
+
+    let ranges = {
+        // generate a random permutation
+        let mut pool: Vec<_> = (0..nranges).collect();
+        let mut ranges = vec![];
+
+        for i in 0..nranges {
+            let k = rng.gen_range(0, nranges-i);
+            let range_idx = pool.swap_remove(k);
+            let from = range_idx * rm_items;
+            let to = ::std::cmp::min(from + rm_items, n);
+            ranges.push((from, to));
+        }
+
+        ranges
+    };
+
+
+    let tree = Tree::new(elems);
+    let mut copy = tree.clone();
+    let mut output = Vec::with_capacity(tree.size());
+
+    let start = time::SystemTime::now();
+    for _ in 0..iters {
+        copy.refill(&tree);
+        for i in 0..nranges {
+            output.truncate(0);
+            let (ref from, ref to) = ranges[i];
+            copy.delete_range(&mut DriverFromTo::new(*from, *to), &mut output);
+            test::black_box(output.len());
+        }
+    }
+    let elapsed = start.elapsed().unwrap();
+    let avg_nanos = nanos(elapsed) / iters;
+    println!("average time for clone/tear down implicit_tree of {} elements in bulks of {} elements: {}ns", n, rm_items, avg_nanos)
+}
+
+
+
+
 #[inline]
 fn nanos(d: Duration) -> u64 {
     d.as_secs()*1000000000 + d.subsec_nanos() as u64
@@ -148,7 +195,10 @@ fn nanos(d: Duration) -> u64 {
 
 
 fn main() {
-    imptree_delete_range_n(100, 100, 10000000);
+//    imptree_delete_range_n(100, 100, 10000000);
+
+
+    imptree_teardown_cycle(100000, 100, 5000);
 
 
     imptree_delete_range_n(100, 100, 5000000);
